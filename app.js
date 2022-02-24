07f2d1c89ca25e9ca02b532fec57274d9904bd45
schillerandrew <hyogoetophile@gmail.com>
2022-02-23 16:06:17 -0800

    adding JS files for more dynamic site

---
+++ app.js
@@ -0,0 +1,8 @@
+let colorQuestion = prompt("So this page has a brown background -- I dunno, I kinda like it, I think it kind of goes along with the bear theme. Do you want to change the color?");
+
+if (colorQuestion == "yes") {
+    prompt("Well too bad, I like it the way it is!")
+}
+// else {
+    // prompt("Yeah I agree -- this brown is the best color for now.")
+// }
---
+++ app3.js
@@ -0,0 +1,3 @@
+let nameQuestion = prompt("Also, could you type in your name down there? Everyone likes to be greeted by inanimate objects!");
+
+document.write("Welcome " + nameQuestion + "! Sorry if this interaction has been awkward -- I hope you like the rest of my site!")
